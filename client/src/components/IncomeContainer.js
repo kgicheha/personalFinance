5f8e9d73502945e604179158fa6dc90f1bc5d2e2
Kevin Gicheha <gichehakevin@gmail.com>
2022-11-30 21:05:52 -0800

    added income container

--- client/src/components/IncomeContainer.js
+++ client/src/components/IncomeContainer.js
@@ -1,9 +1,59 @@
-import React from 'react'
+import React from "react";
+import IncomeDetails from "./IncomeDetails";
+import { styled } from "@mui/material/styles";
+import {
+  Table,
+  TableBody,
+  TableContainer,
+  TableHead,
+  TableRow,
+  Paper,
+} from "@mui/material";
+import TableCell, { tableCellClasses } from "@mui/material/TableCell";
 
+/*
+catergory: salary, investments
+*/
 const IncomeContainer = () => {
-  return (
-    <div>IncomeContainer</div>
-  )
-}
+  const income = [
+    {
+        id: 1,
+        description: "Salary",
+        amount: 5000.00,
+        category: "salary",
+        recurring: true,
+    },
+    {
+        id: 2,
+        description: "Salary",
+        amount: 300.00,
+        category: "investments",
+        recurring: true,
+    },
+];
 
-export default IncomeContainer
+const renderIncome = () =>{
+    income.map((inc) => (
+        <IncomeDetails key= {inc.id} inc={inc}/>
+    ))
+}
+  return (
+    <>
+    <>
+      <TableContainer component={Paper}>
+        <Table sx={{ minWidth: 100 }} aria-label="customized table">
+          <TableHead>
+            <TableRow>
+              <TableCell id="incomeTableHeader">EXPENSES</TableCell>
+            </TableRow>
+          </TableHead>
+        </Table>
+      </TableContainer>
+    </>
+
+    {renderIncome()}
+  </>
+);
+};
+
+export default IncomeContainer;
---
+++ client/src/components/IncomeDetails.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const IncomeDetails = () => {
+  return (
+    <div>IncomeDetails</div>
+  )
+}
+
+export default IncomeDetails
