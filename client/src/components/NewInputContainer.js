e20d1fe310844a80fb59c45b417b540054d97140
Kevin Gicheha <gichehakevin@gmail.com>
2022-12-01 15:56:23 -0800

    added search component

--- client/src/components/NewInputContainer.js
+++ client/src/components/NewInputContainer.js
@@ -1,8 +1,29 @@
 import React from 'react'
+import {Box, InputLabel, MenuItem, FormControl,Select, TextField } from '@mui/material';
+
 
 const NewInputContainer = () => {
+    const handleChange =()=>{
+        console.log("Hello")
+    }
   return (
-    <div>NewInputContainer</div>
+    <Box sx={{ minWidth: 120 }}>
+    <FormControl fullWidth>
+      {/* <InputLabel id="type">Type</InputLabel> */}
+      <Select
+        labelId="demo-simple-select-label"
+        id="demo-simple-select"
+        // value={type}
+        label="Type"
+        onChange={handleChange}
+      >
+        <MenuItem>-</MenuItem>
+        <MenuItem>+</MenuItem>
+      </Select>
+      <TextField id="addDescription" label="Add description" variant="outlined" />
+      <TextField id="addValue" label="value" variant="outlined" />
+    </FormControl>
+  </Box>
   )
 }
 
