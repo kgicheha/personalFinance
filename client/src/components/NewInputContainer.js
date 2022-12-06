619a6b06ed1cbd44171dccdccfa2e01116d6d877
Kevin Gicheha <gichehakevin@gmail.com>
2022-12-06 10:46:00 -0800

    improved search section

--- client/src/components/NewInputContainer.js
+++ client/src/components/NewInputContainer.js
@@ -8,29 +8,50 @@ import {
   TextField,
   Input,
 } from "@mui/material";
+import { useForm } from "react-hook-form";
 
 const NewInputContainer = () => {
+  //storing data from the form
+  const { register, handleSubmit } = useForm();
+
   const handleChange = () => {
     console.log("Hello");
   };
+  const onSubmit = () => {
+    console.log("Hello");
+  };
   return (
-      <FormControl>
+    <form onSubmit={handleSubmit(onSubmit)}>
       <InputLabel>Select</InputLabel>
-        <Select label="Select" onChange={handleChange}>
-          <MenuItem id="income">+</MenuItem>
-          <MenuItem id="expense">-</MenuItem>
-        </Select>
-        <Input
-          id="addDescription"
+      <Select
+        id="addType"
+        label="Select"
+        {...register("type")}
+        onChange={handleChange}
+      >
+        <MenuItem id="income" value="positive">
+          +
+        </MenuItem>
+        <MenuItem id="expense" value="negative">
+          -
+        </MenuItem>
+      </Select>
+      <div id="addDescription">
+        <TextField
           placeholder="Add description"
-          // sx={{size:"large"}}
+          variant="outlined"
+          {...register("description")}
         />
+      </div>
+      <div id="addValue">
         <Input
-          id="addValue"
-          placeholder="value"
+          placeholder="amount"
           type="number"
+          variant="outlined"
+          {...register("amount")}
         />
-      </FormControl>
+      </div>
+    </form>
   );
 };
 
