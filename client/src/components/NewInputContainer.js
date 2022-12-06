f055543914f2c71a81e8973c66fc016555dadd5d
Kevin Gicheha <gichehakevin@gmail.com>
2022-12-05 20:41:28 -0800

    improved search section

--- client/src/components/NewInputContainer.js
+++ client/src/components/NewInputContainer.js
@@ -1,29 +1,37 @@
-import React from 'react'
-import {Box, InputLabel, MenuItem, FormControl,Select, TextField } from '@mui/material';
-
+import React from "react";
+import {
+  Box,
+  InputLabel,
+  MenuItem,
+  FormControl,
+  Select,
+  TextField,
+} from "@mui/material";
 
 const NewInputContainer = () => {
-    const handleChange =()=>{
-        console.log("Hello")
-    }
+  const handleChange = () => {
+    console.log("Hello");
+  };
   return (
     <Box sx={{ minWidth: 120 }}>
-    <FormControl fullWidth>
-      <Select
-        labelId="demo-simple-select-label"
-        id="demo-simple-select"
-        // value={type}
-        label="Type"
-        onChange={handleChange}
-      >
-        <MenuItem>-</MenuItem>
-        <MenuItem>+</MenuItem>
-      </Select>
-      <TextField id="addDescription" placeholder="Add description" variant="outlined" />
-      <TextField id="addValue" placeholder="value" variant="outlined" />
-    </FormControl>
-  </Box>
-  )
-}
+      <FormControl fullWidth>
+        <Select
+          onChange={handleChange}
+          displayEmpty
+          inputProps={{ "aria-label": "Without label" }}
+        >
+          <MenuItem>+</MenuItem>
+          <MenuItem>-</MenuItem>
+        </Select>
+        <TextField
+          id="addDescription"
+          placeholder="Add description"
+          variant="outlined"
+        />
+        <TextField id="addValue" placeholder="value" variant="outlined" />
+      </FormControl>
+    </Box>
+  );
+};
 
-export default NewInputContainer
+export default NewInputContainer;
