1d6c9d82476579ba7baef10e418fe2ade693b6b8
Kevin Gicheha <gichehakevin@gmail.com>
2022-12-07 14:52:49 -0800

    -

--- client/src/components/MenuBar.js
+++ client/src/components/MenuBar.js
@@ -1,130 +1,41 @@
 import React, { useState } from "react";
-import {
-  AppBar,
-  Box,
-  Toolbar,
-  IconButton,
-  Typography,
-  Menu,
-  Container,
-  Button,
-  MenuItem,
-  Link,
-} from "@mui/material";
-import MenuIcon from "@mui/icons-material/Menu";
-import { styled } from "@mui/material/styles";
+import {Tab, Tabs, Box, Button} from '@mui/material';
+
 
 const MenuBar = () => {
-  const pages = ["OVERVIEW", "TRANSACTIONS", "GOALS", "BUDGETS"];
-  const [anchorElNav, setAnchorElNav] = useState(null);
-  const [anchorElUser, setAnchorElUser] = useState(null);
+  const [value, setValue] = useState(0);
 
-  const handleMenuChange =(event) =>{
-    console.log(event.currentTarget)
-  }
-
-  const handleOpenNavMenu = (event) => {
-    setAnchorElNav(event.currentTarget);
-  };
-  const handleOpenUserMenu = (event) => {
-    setAnchorElUser(event.currentTarget);
-  };
-
-  const handleCloseNavMenu = () => {
-    setAnchorElNav(null);
-  };
-
-  const handleCloseUserMenu = () => {
-    setAnchorElUser(null);
+  const handleChange = (event, newValue ) => {
+    console.log(newValue)
+    setValue(newValue);
   };
 
   // CUSTOM CSS
-  const CustomButton = styled(Button)({
-    "&:hover": {
-      textDecoration: "underline",
-      color:"#178fff",
-      backgroundColor: "#f6f6f6",
-      fontWeight: "bold",
-    },
-  });
+  // const CustomButton = styled(Button)({
+  //   "&:hover": {
+  //     textDecoration: "underline",
+  //     color: "#178fff",
+  //     backgroundColor: "#f6f6f6",
+  //     fontWeight: "bold",
+  //   },
+  // });
   //CUSTOM CSS
   return (
     <div>
-      <AppBar position="relative" id="menuBar">
-        <Container maxWidth="xl">
-          <Toolbar>
-          <Link to="/home">
-
-          </Link>
-            <Box sx={{ flexGrow: 1, display: { xs: "flex", md: "none" } }}>
-              <IconButton
-                size="large"
-                aria-label="account of current user"
-                aria-controls="menu-appbar"
-                aria-haspopup="true"
-                onClick={handleOpenNavMenu}
-                color="inherit"
-              >
-                <MenuIcon />
-              </IconButton>
-              <Menu
-                id="menu-appbar"
-                anchorEl={anchorElNav}
-                anchorOrigin={{
-                  vertical: "bottom",
-                  horizontal: "left",
-                }}
-                keepMounted
-                transformOrigin={{
-                  vertical: "top",
-                  horizontal: "left",
-                }}
-                open={Boolean(anchorElNav)}
-                onClose={handleCloseNavMenu}
-                sx={{
-                  display: { xs: "block", md: "none" },
-                }}
-              >
-                {pages.map((page) => (
-                  <MenuItem key={page} onClick={handleMenuChange}>
-                    <Typography textAlign="center">{page}</Typography>
-                  </MenuItem>
-                ))}
-              </Menu>
-            </Box>
-            <Box sx={{ flexGrow: 1, display: { xs: "none", md: "flex" } }}>
-              {pages.map((page) => (
-                <CustomButton
-                  key={page}
-                  onClick={handleMenuChange}
-                  sx={{ my: 2, color: "black", display: "block" }}
-                >
-                  {page}
-                </CustomButton>
-              ))}
-            </Box>
-
-            <Box sx={{ flexGrow: 0 }}>
-              <Menu
-                sx={{ mt: "45px" }}
-                id="menu-appbar"
-                anchorEl={anchorElUser}
-                anchorOrigin={{
-                  vertical: "top",
-                  horizontal: "right",
-                }}
-                keepMounted
-                transformOrigin={{
-                  vertical: "top",
-                  horizontal: "right",
-                }}
-                open={Boolean(anchorElUser)}
-                onClose={handleCloseUserMenu}
-              ></Menu>
-            </Box>
-          </Toolbar>
-        </Container>
-      </AppBar>
+    <Box sx={{ width: '100%' }}>
+    <Tabs
+      value={value}
+      onChange={handleChange}
+      textColor="secondary"
+      indicatorColor="secondary"
+      aria-label="secondary tabs example"
+      centered
+    >
+      <Tab value="OVERVIEW" label="OVERVIEW" />
+      <Tab value="TRANSACTIONS" label="TRANSACTIONS" />
+      <Tab value="GOALS" label="GOALS" />
+    </Tabs>
+  </Box>
     </div>
   );
 };
